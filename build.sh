cf17cd4943ae28a0904aa8d2c485c1d883459c23
zxr <zxr13027502030@163.com>
2019-07-28 14:59:06 +0800

    xxx

--- build.sh
+++ build.sh
@@ -13,3 +13,45 @@ SHA256_VAL=14489086c4feb9585f3884821885ffca8a1e56b010aab5199c4ec0c7908690f5
 check_and_get_source_package $FILE $URL $SHA256_VAL
 check_and_unpackage_source $FILE $DIR
 popd
+
+sudo apt-get build-dep qemu 
+sudo apt-get remove oss4-dev
+sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib \
+     build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
+     xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev \
+     xterm
+sudo apt-get install make xsltproc docbook-utils fop dblatex xmlto
+
+sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib \
+    build-essential chrpath socat libsdl1.2-dev
+
+sudo apt-get install libsdl1.2-dev xterm sed cvs subversion coreutils texi2html \
+    docbook-utils python-pysqlite2 help2man make gcc g++ desktop-file-utils \
+    libgl1-mesa-dev libglu1-mesa-dev mercurial autoconf automake groff curl lzop asciidoc
+
+sudo apt-get install u-boot-tools
+
+if [ ! -d ~/bin ];then
+    mkdir ~/bin
+fi
+
+if [ ! -f ~/bin/repo ];then
+    curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+    chmod a+x ~/bin/repo
+    export PATH=~/bin:$PATH
+fi
+if [ ! -d fsl-release-bsp ];then
+    mkdir fsl-release-bsp
+fi
+which repo
+
+if [ $? == 1 ];then
+    repo
+    exit -1
+fi
+pushd fsl-release-bsp
+repo init -u git://git.freescale.com/imx/fsl-arm-yocto-bsp.git -b imx-4.1-krogoth
+repo sync
+
+DISTRO=fsl-imx-x11 MACHINE=imx6qpsabresd source fsl-setup-release.sh -b build_x11
+popd #fsl-release-bsp
