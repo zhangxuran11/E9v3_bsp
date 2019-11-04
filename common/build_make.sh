1735feddf11520064963bc67bd49c66dcf56c32b
zxr <zxr@163.com>
2019-11-05 01:26:54 +0800

    xxx

--- common/build_make.sh
+++ common/build_make.sh
@@ -8,4 +8,5 @@ source fsl-setup-release.sh -b build_x11
 fi
 DISTRO=fsl-imx-fb MACHINE=imx6qsabresd bitbake core-image-minimal -c populate_sdk 
 bitbake core-image-minimal
+sudo tmp/deploy/sdk/fsl-imx-fb-glibc-x86_64-core-image-minimal-cortexa9hf-neon-toolchain-4.14-sumo.sh
 popd #fsl-release-bsp
--- common/build_uboot.sh
+++ common/build_uboot.sh
@@ -8,11 +8,17 @@ if [[ -d $YOCTO_UBOOT_DIR ]] && [[ ! -d $UBOOT_DIR ]];then
     mkdir $UBOOT_DIR -p
     cp $YOCTO_UBOOT_DIR/* $UBOOT_DIR/ -rfd
 fi
+#exit 0
 if [ ! -d ${UBOOT_DIR}/board/embedsky/tqe9v3 ];then
     mkdir ${UBOOT_DIR}/board/embedsky/tqe9v3 -p
     cp ${UBOOT_DIR}/board/freescale/mx6sabresd/* ${UBOOT_DIR}/board/embedsky/tqe9v3/ -rfd
     rm ${UBOOT_DIR}/board/embedsky/tqe9v3/mx6sabresd.c
     cp resource/uboot_$BSP_VERSION/* $UBOOT_DIR/ -rfd
 fi
-
+pushd $UBOOT_DIR
+source /opt/fsl-imx-fb/4.14-sumo/environment-setup-cortexa9hf-neon-poky-linux-gnueabi
+make clean
+ARCH=arm make tqe9v3_defconfig
+ARCH=arm make
+popd
 
